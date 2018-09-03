cad52975b7bac10cc14a7a8d8292b21b4ad48f84
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-09-03 22:24:31 +0900

    programmers algorithm exercise level1 : 자연수 뒤집어 배열로 만들기
    list와 Collection, Arrays 객체를 이용한 방법으로 두 번쨰 구현

---
+++ algorithm/src/level1/ReverseNumberArray2.java
@@ -0,0 +1,37 @@
+/*자연수 뒤집어 배열로 만들기
+문제 설명
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+제한 조건
+n은 10,000,000,000이하인 자연수입니다.
+입출력 예
+n	return
+12345	[5,4,3,2,1]*/
+package level1;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseNumberArray2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReverseNumberArray2 rna = new ReverseNumberArray2();
+		System.out.println(rna.solution(12345)[0]);
+		
+	}
+	
+	public int[] solution(long n) {
+		int[] answer = {};
+		List<String> list = new ArrayList<>();
+		String[] answerString = Long.toString(n).split("");
+		list = Arrays.asList(answerString);
+		Collections.reverse(list);
+		answer = list.stream().mapToInt(i -> Integer.parseInt(i)).toArray();
+	    return answer;
+	}
+
+}
