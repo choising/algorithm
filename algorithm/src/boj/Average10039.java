c2f5f92058f47b52c904b6ae421c75f0b0e6d328
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-09-29 23:25:47 +0900

    boj 10039 : 평균 점수

---
+++ algorithm/src/boj/Average10039.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Average10039 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+			if(arr[i] < 40) arr[i] = 40;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		
+		System.out.println(sum/5);
+		sc.close();
+		
+		
+	}
+
+}
