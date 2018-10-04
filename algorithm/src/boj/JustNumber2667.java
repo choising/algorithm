e517044034c7713de80aa4d81eeb3c21d4766f64
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-10-05 00:27:34 +0900

    boj 2667 : 단지번호붙이기
    BFS를 이용하여 풀긴 했는데 코드가 너무 긴 것 같다.

---
+++ algorithm/src/boj/JustNumber2667.java
@@ -0,0 +1,110 @@
+package boj;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class JustNumber2667 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		String[][] sArr = new String[n][n];
+		for(int i = 0; i < n; i++) {
+			sArr[i] = sc.next().split("");
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(sArr[i][j]);
+			}
+		}
+
+		sc.close();
+		
+		boolean visit[][] = new boolean[arr.length][arr.length];
+		List<Integer> list = new ArrayList<>();
+		Queue<int[]> q = new LinkedList<>();
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				if(arr[i][j] == 1 && visit[i][j] == false) {
+					visit[i][j] = true;
+					int[] qArr = new int[2];
+					qArr[0] = i;
+					qArr[1] = j;
+					q.add(qArr);
+					bfs(arr, visit, q, 0, list);
+				}
+			}
+		}
+		
+		System.out.println(list.size());
+		Collections.sort(list);
+		for(int intVal : list) {
+			System.out.println(intVal);
+		}
+	}
+	
+
+	public static void bfs(int[][] arr, boolean[][] visit, Queue<int[]> q, int cnt, List<Integer> list) {
+		if(q.isEmpty()) {
+			list.add(cnt);
+			return;
+		}
+		int[] qArr = q.poll();
+		cnt += 1;
+		if(qArr[0] != 0) {
+			int x = qArr[0] - 1;
+			int y = qArr[1];
+			if(arr[x][y] == 1 && visit[x][y] == false) {
+				visit[x][y] = true;
+				int[] newArr = new int[2];
+				newArr[0] = x;
+				newArr[1] = y;
+				q.add(newArr);
+			}
+		}
+		if(qArr[0] != arr.length - 1) {
+			int x = qArr[0] + 1;
+			int y = qArr[1];
+			if(arr[x][y] == 1 && visit[x][y] == false) {
+				visit[x][y] = true;
+				int[] newArr = new int[2];
+				newArr[0] = x;
+				newArr[1] = y;
+				q.add(newArr);
+			}
+		}
+		if(qArr[1] != 0) {
+			int x = qArr[0];
+			int y = qArr[1] - 1;
+			if(arr[x][y] == 1 && visit[x][y] == false) {
+				visit[x][y] = true;
+				int[] newArr = new int[2];
+				newArr[0] = x;
+				newArr[1] = y;
+				q.add(newArr);
+			}
+		}
+		if(qArr[1] != arr.length - 1) {
+			int x = qArr[0];
+			int y = qArr[1] + 1;
+			if(arr[x][y] == 1 && visit[x][y] == false) {
+				visit[x][y] = true;
+				int[] newArr = new int[2];
+				newArr[0] = x;
+				newArr[1] = y;
+				q.add(newArr);
+			}
+		}
+		bfs(arr, visit, q, cnt, list);
+	}
+	
+	
+}
