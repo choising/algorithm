a557c6e74df69a6b9a289ea3957baaac2956b4b2
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2018-10-05 11:43:09 +0900

    boj 2667 : line 수 절감.

--- algorithm/src/boj/JustNumber2667.java
+++ algorithm/src/boj/JustNumber2667.java
@@ -13,26 +13,25 @@ public class JustNumber2667 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[][] arr = new int[n][n];
-		String[][] sArr = new String[n][n];
-		for(int i = 0; i < n; i++) {
+		int[][] arr = new int[n+2][n+2];
+		String[][] sArr = new String[n+2][n+2];
+		for(int i = 1; i <= n; i++) {
 			sArr[i] = sc.next().split("");
 		}
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				arr[i][j] = Integer.parseInt(sArr[i][j]);
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(sArr[i][j-1]);
 			}
 		}
 
 		sc.close();
-		
 		boolean visit[][] = new boolean[arr.length][arr.length];
 		List<Integer> list = new ArrayList<>();
 		Queue<int[]> q = new LinkedList<>();
 		
-		for(int i = 0; i < arr.length; i++) {
-			for(int j = 0; j < arr.length; j++) {
+		for(int i = 1; i < arr.length; i++) {
+			for(int j = 1; j < arr.length; j++) {
 				if(arr[i][j] == 1 && visit[i][j] == false) {
 					visit[i][j] = true;
 					int[] qArr = new int[2];
@@ -59,42 +58,12 @@ public class JustNumber2667 {
 		}
 		int[] qArr = q.poll();
 		cnt += 1;
-		if(qArr[0] != 0) {
-			int x = qArr[0] - 1;
-			int y = qArr[1];
-			if(arr[x][y] == 1 && visit[x][y] == false) {
-				visit[x][y] = true;
-				int[] newArr = new int[2];
-				newArr[0] = x;
-				newArr[1] = y;
-				q.add(newArr);
-			}
-		}
-		if(qArr[0] != arr.length - 1) {
-			int x = qArr[0] + 1;
-			int y = qArr[1];
-			if(arr[x][y] == 1 && visit[x][y] == false) {
-				visit[x][y] = true;
-				int[] newArr = new int[2];
-				newArr[0] = x;
-				newArr[1] = y;
-				q.add(newArr);
-			}
-		}
-		if(qArr[1] != 0) {
-			int x = qArr[0];
-			int y = qArr[1] - 1;
-			if(arr[x][y] == 1 && visit[x][y] == false) {
-				visit[x][y] = true;
-				int[] newArr = new int[2];
-				newArr[0] = x;
-				newArr[1] = y;
-				q.add(newArr);
-			}
-		}
-		if(qArr[1] != arr.length - 1) {
-			int x = qArr[0];
-			int y = qArr[1] + 1;
+		int[] minusX = {-1, 1, 0, 0};
+		int[] minusY = {0, 0, -1, 1};
+		
+		for(int i = 0; i < 4; i++) {
+			int x = qArr[0] + minusX[i];
+			int y = qArr[1] + minusY[i];
 			if(arr[x][y] == 1 && visit[x][y] == false) {
 				visit[x][y] = true;
 				int[] newArr = new int[2];
@@ -105,6 +74,4 @@ public class JustNumber2667 {
 		}
 		bfs(arr, visit, q, cnt, list);
 	}
-	
-	
 }
