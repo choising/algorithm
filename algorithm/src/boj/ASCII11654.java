e31362f2400d736b4ec96006a6b4ec83c29f3ae9
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-10-06 23:44:06 +0900

    boj 11654 : 아스키 코드

---
+++ algorithm/src/boj/ASCII11654.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.util.Scanner;
+
+public class ASCII11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		sc.close();
+		char c = s.charAt(0);
+		int answer = c+0;
+		System.out.println(answer);
+	}
+
+}
