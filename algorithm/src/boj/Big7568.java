9f379d8f7a921df86a59555d4fe7d8cf58f4e7a6
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-10-18 23:55:06 +0900

    boj 7568 : 덩치
    initial

---
+++ algorithm/src/boj/Big7568.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Big7568 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][2];
+		for(int i = 0; i < n; i++) {
+			String[] sArr = br.readLine().split(" ");
+			
+		}
+	}
+
+}
