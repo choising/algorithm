8915c91406f09cd1215851ed95f9ad6421cdf3c4
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-10-19 01:16:05 +0900

    boj 7568 : 덩치

--- algorithm/src/boj/Big7568.java
+++ algorithm/src/boj/Big7568.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Big7568 {
+public class Big7568{
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
@@ -14,8 +14,18 @@ public class Big7568 {
 		int[][] arr = new int[n][2];
 		for(int i = 0; i < n; i++) {
 			String[] sArr = br.readLine().split(" ");
-			
+			int[] iArr = Arrays.stream(sArr).mapToInt(s -> Integer.parseInt(s)).toArray();
+			arr[i] = iArr;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			int count = 1;
+			for(int j = 0; j < arr.length; j++) {
+				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					count++;
+				}
+			}
+			System.out.print(count + " ");
 		}
 	}
-
 }
