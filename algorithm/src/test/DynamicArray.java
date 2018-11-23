e30b90d14f220e0e8b5af7acab7dfa81dea690fd
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2018-11-23 10:45:56 +0900

    동적 배열 ADT 구현

---
+++ algorithm/src/test/DynamicArray.java
@@ -0,0 +1,80 @@
+package test;
+
+public class DynamicArray<E> {
+	private static int DEFAULT_SIZE = 10;
+	
+	private int size;
+	private Object[] arr;
+	
+	public DynamicArray() {
+		this.size = 0;
+		this.arr = new Object[DEFAULT_SIZE];
+	}
+	
+	public DynamicArray(int size) {
+		if(size < 0) throw new IllegalArgumentException();
+		this.size = size;
+		this.arr = new Object[this.size];
+	}
+
+	public E get(int index) {
+		if(index >= this.size) throw new IllegalArgumentException();
+		return (E)this.arr[index];
+	}
+
+	public int size() {
+		return this.size;
+	}
+	
+	public void set(int index, E value) {
+		if(index >= this.arr.length) {
+			int newSize = this.newSize(index);
+			this.sizeUp(newSize);
+		}
+		if(index+1 > this.size) this.size = index+1; 
+		this.arr[index] = value;		
+	}
+
+	public E remove(int index) {
+		if(index >= this.size) throw new IllegalArgumentException();
+		E value = (E)this.arr[index];
+		this.shiftLeft(index);		
+		return value;
+	}
+	
+	private void shiftLeft(int index) {
+		this.size--;
+		for(int i = index; i < this.size; i++) {
+			this.arr[i] = this.arr[i+1];
+		}
+	}
+	
+	private int newSize(int index) {
+		int curSize = this.arr.length;
+		while(index >= curSize) curSize *= 2;
+		return curSize;
+	}
+	
+	private void sizeUp(int newSize) {
+		Object[] temp = new Object[newSize];
+		for(int i = 0; i < this.size; i++) {
+			temp[i] = this.arr[i];
+		}
+		this.arr = temp;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for(int i = 0; i < this.size - 1; i++) {
+			sb.append(this.arr[i] + ", ");
+		}
+		sb.append(this.arr[this.size-1] + "]");
+		return sb.toString();
+	}
+	
+	
+
+}
---
+++ algorithm/src/test/DynamicArrayTest.java
@@ -0,0 +1,68 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class DynamicArrayTest {
+	@Test
+	public void init() {
+		DynamicArray<Integer> da = new DynamicArray<>();
+		assertEquals(0, da.size());
+		
+		int size = 15;
+		DynamicArray<Integer> da2 = new DynamicArray<>(size);
+		assertEquals(size, da2.size());
+		
+		size = 5;
+		DynamicArray<Integer> da3 = new DynamicArray<>(size);
+		assertEquals(size, da3.size());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void getWithException() {
+		DynamicArray<Integer> da = new DynamicArray<>();
+		da.get(11);
+	}
+	
+	@Test
+	public void get() {
+		DynamicArray<Integer> da = new DynamicArray<>();
+		da.set(0, 1);
+		assertEquals((Integer)1, da.get(0));
+	}
+	
+	@Test
+	public void set() {
+		DynamicArray<Integer> da = new DynamicArray<>();
+		da.set(0, 1);
+		assertEquals((Integer)1, da.get(0));
+		
+		da.set(10, 5);
+		assertEquals((Integer)5, da.get(10));
+		assertEquals(11, da.size());
+		
+		DynamicArray<String> da2 = new DynamicArray<>();
+		da2.set(0, "Hello");
+		assertEquals("Hello", da2.get(0));
+	}
+	
+	@Test
+	public void remove() {
+		DynamicArray<Integer> da = new DynamicArray<>();
+		da.set(0, 0);
+		da.set(1, 1);
+		da.set(2, 2);
+		da.set(3, 3);
+		da.set(4, 4);
+		da.set(5, 5);
+		assertEquals(6, da.size());
+		assertEquals("[0, 1, 2, 3, 4, 5]", da.toString());
+		assertEquals((Integer)3, da.remove(3));
+		assertEquals(5, da.size());
+		assertEquals("[0, 1, 2, 4, 5]", da.toString());
+	}
+	
+	
+
+}
