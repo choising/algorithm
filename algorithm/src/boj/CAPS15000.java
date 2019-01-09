06ccda1a0b83506d00f5bb30a470ac1a67f2e4f9
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2019-01-09 22:23:52 +0900

    boj 15000 : CAPS

---
+++ algorithm/src/boj/CAPS15000.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CAPS15000 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s.toUpperCase());
+	}
+}
