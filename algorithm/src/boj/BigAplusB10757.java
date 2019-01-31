48b4cd4e3ac538af8b64e6600135a1882e39eb9a
LAPTOP-E3LTPJD6\ChoiSing <chltmdals115@gmail.com>
2019-01-31 18:13:23 +0900

    boj 10757 : 큰  수 A+B

---
+++ algorithm/src/boj/BigAplusB10757.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BigAplusB10757 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String a = st.nextToken();
+		String b = st.nextToken();
+		
+		BigInteger a1 = new BigInteger(a);
+		BigInteger a2 = new BigInteger(b);
+		
+		System.out.println(a1.add(a2).toString());
+	}
+
+}
