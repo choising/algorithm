bfaf7e0d63621b909e68b323c9237032edc7d7a3
Seungmin <Seungmin@Seungwonui-MacBook.local>
2019-02-09 22:36:37 +0900

    boj 1075 : 나누기

---
+++ algorithm/src/boj/Divide1075.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Divide1075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+
+        int a = n - n%100;
+        System.out.println(go(a,f));
+    }
+
+    private static String go(int a, int f) {
+        for(int i = a; i < a+100; i++) {
+            if(i % f == 0) {
+                int answer = i%100;
+                return answer < 10 ? "0" + answer : String.valueOf(answer);
+            }
+        }
+        return "";
+    }
+}
