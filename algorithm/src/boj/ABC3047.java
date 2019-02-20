01791df839b7297a90e2c18e8548f1f618704fe5
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-02-20 23:27:52 +0900

    boj 3047 : ABC

---
+++ algorithm/src/boj/ABC3047.java
@@ -0,0 +1,49 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class ABC3047 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int[] arr = new int[3];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		String ss = br.readLine();
+		for(String s : ss.split("")) {
+			System.out.print(arr[MyNumber.find(s)] + " ");
+		}
+	}
+}
+
+enum MyNumber {
+	AA(0, "A"), BB(1, "B"), CC(2, "C");
+	
+	private int val;
+	private String str;
+
+	private MyNumber(int val, String str) {
+		this.val = val;
+		this.str = str;
+	}
+	
+	public int getVal() {
+		return val;
+	}
+
+	public static int find(String s) {
+		for (MyNumber n : values()) {
+			if(n.str.equals(s))
+				return n.val;
+		}
+		return 0;
+	}
+}
