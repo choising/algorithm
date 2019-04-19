f39412d4eba9967a4e15a889849dbc0673743c31
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-04-19 21:33:32 +0900

    사용하지않는 import 문 삭제

--- algorithm/src/boj/League5544.java
+++ algorithm/src/boj/League5544.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -40,7 +39,6 @@ public class League5544 {
 		}
 		
 		Collections.sort(list);
-		System.out.println(list);
 		
 		int[] rank = new int[n+1];
 		int r = 1;
